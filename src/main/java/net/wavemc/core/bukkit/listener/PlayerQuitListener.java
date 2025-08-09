069553ec1953fbc8ae8754476ec9e1ef9916cf8b
RAFAELESPADA <pidgeypidgey1@gmail.com>
2025-08-09 17:20:01 -0300

    4.0.3 fix 5

--- src/main/java/net/wavemc/core/bukkit/listener/PlayerQuitListener.java
+++ src/main/java/net/wavemc/core/bukkit/listener/PlayerQuitListener.java
@@ -9,6 +9,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 import java.util.Objects;
+import java.util.UUID;
 
 @AllArgsConstructor
 public class PlayerQuitListener implements Listener {
@@ -19,7 +20,7 @@ public class PlayerQuitListener implements Listener {
         Player player = event.getPlayer();
         WavePlayer helixPlayer = WaveBukkit.getInstance().getPlayerManager().getPlayer(player.getName());
         if (!Objects.equals(helixPlayer.getPvp().getUuid(), event.getPlayer().getUniqueId().toString())) {
-            helixPlayer.getPvp().setUuid(event.getPlayer().getUniqueId().toString());
+            helixPlayer.getPvp().setUuid(UUID.fromString(event.getPlayer().getUniqueId().toString()));
         }
         WaveBukkit.getPlayerController().save(helixPlayer);
     }
